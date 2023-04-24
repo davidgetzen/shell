22234a249ba7d5d089c181a2bedcc8b17b0a0fb7
david-getzen <david.getzen@siegetrading.com>
2023-04-24 13:57:39 -0500

    almost finished with parsing and tokenizing functions

---
+++ about.md
@@ -0,0 +1,3 @@
+Q: Why make this project?
+A: I wanted to acquaint myself with C, low-level programming, and complete 
+a project using Vim. 
---
+++ main.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#define LSH_RL_BUFSIZE 1024
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+//Little note: in C, the keyword 'void' must actually be a parameter of the function header. Otherwise, it can accept an arbitrary number of arguments. 
+char **lsh_split_line(char *line){
+	int bufsize = LSH_TOK_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char*));
+	char *token;
+
+	if(!tokens){
+		fprintf(stderr, "mysh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(line, LSH_TOK_DELIM);
+	while (token != NULL){
+		tokens[position] = token;
+		position++;
+	}
+
+}
+char *mysh_read_line(void){
+	
+	int bufsize = LSH_RL_BUFSIZE;
+	int position = 0;
+	char *buffer = malloc(sizeof(char) * bufsize);
+	int c; 
+
+	if(!buffer){
+		fprint(stderr, "mysh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	while(1){
+		//Reading a character
+		c = getchar();
+
+		//If we hit EOF, replace it with a null character and return
+		//Note: we store c as an int rather than a char, as EOF is an int. If we stored c as a char, we would not be able to check for EOF 
+		if (c == EOF || c == '\n') {
+			buffer[position] = '\0';
+			return buffer;
+		}else{
+			buffer[position] = c;
+		}
+		position++; 
+
+	
+		//If position is out of bounds, reallocate the block with twice the memory space 
+		if (position >= bufsize){
+			bufsize += LSH_RL_BUFSIZE;
+			realloc(buffer, bufsize);
+			if (!buffer){
+				fprintf(stderr, "mysh: allocation error\n");
+				exit(EXIT_FAILURE);
+				}
+			}
+
+		}
+
+
+}
+
+
+void mysh_loop(void){
+
+	char *line;
+	char **args;
+	int status;
+	do {
+		printf("> ");
+		line = mysh_read_line();
+		args = mysh_split_line(line);
+		status = mysh_execute(args);
+
+		free(line);
+		free(args);
+
+	} while(status);
+}
+
+
+
+
+
+int main(int argc, char **argv){	
+	
+	//Load config files
+	
+	//Run command loop
+	mysh_loop();
+
+	//Perform shutdown/cleanup
+
+
+	return 0;
+}
